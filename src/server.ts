f07ee1bcd4649edd993ddba6cc23a69d216a625d
victorjunior95 <victorjr.muchacho@gmail.com>
2021-06-25 10:57:04 -0300

    Day 3 completed

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import "reflect-metadata";
-import express from "express";
-import { router } from "./routes";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 
+import { router } from "./routes";
 import "./database";
 
 // @types/express
@@ -35,6 +36,16 @@ Por isso a necessidade de utilizar o Insomnia ou Postman
 // });
 
 app.use(router);
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({ error: err.message });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+});
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
